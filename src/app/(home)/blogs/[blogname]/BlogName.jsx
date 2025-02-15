84c13af3a8fd5cca73081c829cf33984c4e08e68
AbhishekSharmra <abhishek@coderworldlabs.com>
2025-02-15 12:16:18 +0530

    msg

--- src/app/(home)/blogs/[blogname]/BlogName.jsx
+++ src/app/(home)/blogs/[blogname]/BlogName.jsx
@@ -149,17 +149,17 @@ function BlogName() {
                                        <div className="row gutter_30px">
                                           <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-5 mb-lg-5 mb-xl-0">
                                              <div className="simple_image_boxes" style={{ height: "100%", maxHeight: "390px" }}>
-                                                <img src={data?.image1} className="simp_img img-fluid height_540px" alt="image" />
+                                                <img src={data?.image1} className="simp_img img-fluid height_540px" alt="" />
                                              </div>
                                              <div className="pd_bottom_30"></div>
                                           </div>
                                           <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                              <div className="simple_image_boxes" style={{maxHeight:"180px"}}>
-                                                <img src={data?.image2} className="simp_img img-fluid height_260" alt="image" />
+                                                <img src={data?.image2} className="simp_img img-fluid height_260" alt="" />
                                              </div>
                                              <div className="pd_bottom_30"></div>
                                              <div className="simple_image_boxes " style={{maxHeight:"180px"}}>
-                                                <img src={data?.image3} className="simp_img img-fluid height_250px" alt="image" />
+                                                <img src={data?.image3} className="simp_img img-fluid height_250px" alt="" />
                                              </div>
                                           </div>
                                        </div>
